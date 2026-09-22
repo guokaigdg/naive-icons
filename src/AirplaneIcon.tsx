ff54affc024b85f593dda06413c96a5fb51be5c0
guokaigdg <guokaigdg@gmail.com>
2026-09-22 23:03:46 +0800

    style(icons): refine airplane tail and snowflake geometry

--- src/AirplaneIcon.tsx
+++ src/AirplaneIcon.tsx
@@ -19,7 +19,7 @@ export const AirplaneIcon = forwardRef<SVGSVGElement, IconProps>((props, ref) =>
       {props.title ? <title>{props.title}</title> : null}
 
     
-<path d="M22 8 L26 8 L28 20 L42 22 L42 26 L28 28 L26 40 L22 40 L20 28 L6 26 L6 22 L20 20 Z" fill="#264653"/>
+<path d="M22 8 L26 8 L28 20 L42 22 L42 26 L28 28 L26.7 33 L33 40 L15 40 L20.8 33 L20 28 L6 26 L6 22 L20 20 Z" fill="#264653"/>
 <path d="M22 20 L26 20 L26 28 L22 28 Z" fill="#E76F51"/>
 
   
